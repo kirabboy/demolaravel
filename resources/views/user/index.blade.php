e95f4dd41b7642613d34ec16a2d8a0237127f367
kirabboy <kirabboytt@gmail.com>
2021-09-13 17:13:25 +0700

    testDB

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,17 @@
-@extends('master')
-@section('content')
-<h2>body</h2>
-@endsection
+@foreach ( $products as $product)
+    <b>id:{{$product->id}}</b>
+    <div class="product-box">
+        <div class="product-iamge">
+            <img src="{{$product->feature_img}}" alt="">
+        </div>
+        <div class="product-text">
+            <h3 class="title">{{$product->title}}</h3>
+            <b class="price">{{$product->price}}</b>
+            <div>
+                {!!$product->long_desc!!}
+
+            </div>
+            <button>Mua ngay</button>
+        </div>
+    </div>
+@endforeach
